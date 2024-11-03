b58396ac131a4b48c4cb51ba4a3774cb792e195c
Bytewer <dwx_job@outlook.com>
2024-11-03 18:47:24 +0800

    Update __init__.py

--- akshare/__init__.py
+++ akshare/__init__.py
@@ -2919,7 +2919,7 @@ amac_manager_cancelled_info # 中国证券投资基金业协会-信息公示-诚
 1.15.7 fix: fix index_hist_sw interface
 """
 
-__version__ = "1.15.7"
+__version__ = "1.15.7.1.1.1"
 __author__ = "AKFamily"
 
 import sys
